c55e429bde036f362cc8b5c18a5a4075662d3959
Soumyaranjan Behera <beherasoumyaranjan650>
2025-01-28 15:29:05 +0530

    explore get,post and delete

--- src/app.js
+++ src/app.js
@@ -7,13 +7,25 @@ app.use('/home', (req, res) => {
 
 app.use('/hero', (req, res) => {
     res.send('This is hero section!');
-  });
+});
 
 app.use('/app', (req, res) => {
     res.send('This is app section!');
-  });
+});
 
-app.listen(3000, () => {    
+//This will only handle the GET calls to /user
+app.get('/user', (req, res) => {
+    res.send({ firstName: 'Soumya', lastName: 'ranjan' });
+});
+
+app.post('/user', (req, res) => {
+    res.send('data is saved successfully from data base');
+});
+
+app.delete('/user', (req, res) => {
+    res.send('data is deleted successfully from data base');
+});
+
+app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000');
 });
- 

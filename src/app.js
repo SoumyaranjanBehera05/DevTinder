eb192be8da66300a03ce3ddde6c780e46bdc8295
Soumyaranjan Behera <beherasoumyaranjan650>
2025-02-08 08:41:11 +0530

    Dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -2,15 +2,10 @@ const express = require('express');
 const connectDB = require('./config/database');
 const app = express();
 const User = require('./models/user');
+app.use(express.json());
 
 app.post('/signup', async (req, res) => {
-    const user = new User({
-        firstName: "Piyush",
-        lastName: "sharma",
-        age: 29,
-        email: "beherasoumya650@gmail.com",
-        password: "piyush@134"
-    });
+    const user = new User(req.body);
     try {
         await user.save();
     res.send("User created successfully");
